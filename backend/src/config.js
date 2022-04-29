3c12175258202b76e01d9ff48dd958eef22a14c5
RebelModz <57653239+RebelModz@users.noreply.github.com>
2022-04-29 12:02:44 +0800

    n

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-05-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-05-11T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-11T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 100; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x41feB2D9065cBFaC8f9d9b55FE9f38B59164678d"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
