b2bb3a4008948517065e46b17fd55adcb05f2c92
RebelModz <57653239+RebelModz@users.noreply.github.com>
2022-05-02 12:57:43 +0800

    nnn

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -55,11 +55,17 @@ const updateSocialMediaLinks = (opensea, discord, twitter, instagram, metamask) 
 
 const showToast = (message) => {
   Toastify({
-    text: message,
-    className: "info",
+    text: "This is a toast",
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "top", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
       background: "linear-gradient(to right, #00b09b, #96c93d)",
-    }
+    },
   }).showToast();
 }
 
