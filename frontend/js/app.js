292e08068add8910f30b0d0010678b61c9b924ce
RebelModz <57653239+RebelModz@users.noreply.github.com>
2022-05-02 12:12:19 +0800

    TEST

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -13,7 +13,8 @@ window.addEventListener("load", async () => {
 
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);
-    checkChain();
+    setTimeout(() => checkChain(), 1000);
+    
   } else if (window.web3) {
     window.web3 = new Web3(window.web3.currentProvider);
   }
